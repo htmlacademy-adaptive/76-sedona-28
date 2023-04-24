b735eb6e163fe9f7af995da8cd7c0d7174190209
vpershin1988 <pershinvictor@mail.ru>
2023-04-24 11:03:24 +0300

    Декоративные элементы

---
+++ source/script.js
@@ -0,0 +1,12 @@
+let navMain = document.querySelector('.main-navigation');
+let navToggle = document.querySelector('.main-navigation__toggle');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-navigation--closed')) {
+    navMain.classList.remove('main-navigation--closed');
+    navMain.classList.add('main-navigation--opened');
+  } else {
+    navMain.classList.add('main-navigation--closed');
+    navMain.classList.remove('main-navigation--opened');
+  }
+});
